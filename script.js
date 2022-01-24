91aa1bf719b123adfe8d744fcb95a35ee39561ce
rahulpra045 <98214910+rahulpra045@users.noreply.github.com>
2022-01-24 13:45:02 +0530

    Add files via upload
    to-do list

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
-function getmarks(){
-    var maths = Number(document.getElementById("maths").value);
-    var physics = Number(document.getElementById("physics").value);
-    var chemistry = Number(document.getElementById("chemistry").value);
-    var english = Number(document.getElementById("english").value);
-    var biology = Number(document.getElementById("biology").value);
-    // console.log("This is working");
-    // console.log(maths + physics +chemistry +english +biology);
-    var total = 500;
-    var marksobtained = maths + physics +chemistry +english +biology;
-    var percentage = (marksobtained / total)*100;
-    document.getElementById("percentage").innerHTML = percentage + "%";
-    // console.log(percentage + "%");
-    var grade = "";
-    if(percentage > 90){
-        grade = "A+";
-    }else if(percentage <= 90 && percentage > 80){
-        grade = "A";
+var nodelist = document.getElementsByTagName("LI");
+var i;
+
+var textinput = document.getElementById("input");
+
+textinput.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("mybtn").click();
     }
-    else if(percentage <= 80 && percentage > 70){
-        grade = "B+";
+});
+
+for (i = 0; i < nodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    nodelist[i].appendChild(span);
+}
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    };
+}
+function newElement() {
+    var li = document.createElement("LI");
+    var inputValue = document.getElementById("input").value;
+
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === "") {
+        alert("Text can't be empty");
+    } else {
+        document.getElementById("myul").appendChild(li);
+        li.className = "list-group-item";
     }
-    else if(percentage <= 70 && percentage > 60){
-        grade = "B";
-    }
-    else if(percentage <= 60 && percentage > 50){
-        grade = "c+";
-    }
-    else if(percentage <= 50 && percentage > 40){
-        grade = "C";
-    }
-    else if(percentage <= 40 && percentage > 30){
-        grade = "D";
-    }
-    else if(percentage <= 30 && percentage > 20){
-        grade = "E";
-    }
-    else if(percentage <= 20 && percentage > 0){
-        grade = "F";
-    }
-    document.getElementById("grade").innerHTML = grade;
-    if(percentage < 60){
-        document.getElementById("pass").innerHTML = "Sorry! YOu Failed! Please try next time";
-    }else{
-        document.getElementById("pass").innerHTML = "Congratulation!! You have passed";
+    document.getElementById("input").value = "";
+    var span = document.createElement("SPAN");
+    var text = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(text);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        };
     }
 }
